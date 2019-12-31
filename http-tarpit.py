9fbe25b0d047d30d4212ee65138acaa3994d8a03
hmallen <allenhm@gmail.com>
2019-12-31 16:26:33 -0700

    Added SSH and HTTP tarpits.

---
+++ http-tarpit.py
@@ -0,0 +1,21 @@
+import asyncio
+import random
+
+async def handler(_reader, writer):
+    writer.write(b'HTTP/1.1 200 OK\r\n')
+    try:
+        while True:
+            await asyncio.sleep(5)
+            header = random.randint(0, 2**32)
+            value = random.randint(0, 2**32)
+            writer.write(b'X-%x: %x\r\n' % (header, value))
+            await writer.drain()
+    except ConnectionResetError:
+        pass
+
+async def main():
+    server = await asyncio.start_server(handler, '0.0.0.0', 80)
+    async with server:
+        await server.serve_forever()
+
+asyncio.run(main())
---
+++ ssh-tarpit.py
@@ -0,0 +1,18 @@
+import asyncio
+import random
+
+async def handler(_reader, writer):
+    try:
+        while True:
+            await asyncio.sleep(10)
+            writer.write(b'%x\r\n' % random.randint(0, 2**32))
+            await writer.drain()
+    except ConnectionResetError:
+        pass
+
+async def main():
+    server = await asyncio.start_server(handler, '0.0.0.0', 22)
+    async with server:
+        await server.serve_forever()
+
+asyncio.run(main())
